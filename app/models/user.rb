c075f9398e6936f170c8292e34a3fc14f7eb10bd
jttmckee <wp.jason@hotmail.co.uk>
2019-01-30 17:15:34 +0000

    Authorisation work completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   #Validations
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255},
+  validates :email,  length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence:true, length: {minimum: 6}
+  validates :password, presence:true, length: {minimum: 6}, allow_nil: true
 
   #Enable secure password functionality using BCrypt
   has_secure_password
